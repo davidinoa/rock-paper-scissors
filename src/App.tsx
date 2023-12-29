9b30fbddf1dd7f698acbb36101b6cc34d8842a06
David Inoa <davidinoa@gmail.com>
2023-12-29 13:51:42 -0400

    feat: create state machine with all the game logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
+import { useMachine } from '@xstate/react'
+import machine, { choices } from './machine'
+
 function App() {
+  const [state, send] = useMachine(machine)
+
   return (
-    <div className="bg-black text-white text-4xl h-screen grid place-items-center">
-      Hello World!
+    <div className="grid h-screen place-items-center bg-black text-4xl text-white">
+      {state.matches('start') && (
+        <button
+          type="button"
+          className="rounded-md bg-green-600 px-4 py-2 text-white"
+          onClick={() => send({ type: 'PLAY' })}
+        >
+          Start Game
+        </button>
+      )}
+      {state.matches('playerTurn') &&
+        choices.map((choice) => (
+          <button
+            type="button"
+            className="rounded-md bg-green-600 px-4 py-2 text-white"
+            key={choice}
+            onClick={() => send({ type: 'SELECT', choice })}
+          >
+            {choice}
+          </button>
+        ))}
+      {state.matches('computerTurn') && (
+        <div>
+          <h1>Computer is thinking...</h1>
+        </div>
+      )}
+      {state.matches('end') && (
+        <div>
+          <h1>Game Over</h1>
+          <h2>Computer chose: {state.context.computerChoice}</h2>
+          <h2>Player chose: {state.context.playerChoice}</h2>
+          <h2>Winner: {state.context.winner}</h2>
+          <button
+            type="button"
+            className="rounded-md bg-green-600 px-4 py-2 text-white"
+            onClick={() => send({ type: 'PLAY_AGAIN' })}
+          >
+            Play Again
+          </button>
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/machine.ts
@@ -0,0 +1,104 @@
+import { assign, setup } from 'xstate'
+
+export const choices = ['rock', 'paper', 'scissors'] as const
+
+const determineWinner = (
+  playerChoice: string,
+  computerChoice: string,
+): 'player' | 'computer' | 'draw' => {
+  if (playerChoice === computerChoice) return 'draw'
+  if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    return 'player'
+  }
+  return 'computer'
+}
+
+type Choice = (typeof choices)[number]
+type Winner = 'player' | 'computer' | 'draw'
+
+type Context = {
+  playerChoice: Choice | null
+  computerChoice: Choice | null
+  winner: Winner | null
+}
+
+const initialContext: Context = {
+  playerChoice: null,
+  computerChoice: null,
+  winner: null,
+}
+
+type Event =
+  | { type: 'PLAY' }
+  | { type: 'SELECT'; choice: Choice }
+  | { type: 'PLAY_AGAIN' }
+
+const machine = setup({
+  types: {} as {
+    context: Context
+    events: Event
+  },
+}).createMachine({
+  id: 'rockPaperScissors',
+  initial: 'start',
+  context: initialContext,
+  states: {
+    start: {
+      on: {
+        PLAY: {
+          target: 'playerTurn',
+        },
+      },
+    },
+    playerTurn: {
+      on: {
+        SELECT: {
+          target: 'computerTurn',
+          actions: [
+            assign({
+              playerChoice: ({ event }) => event.choice,
+            }),
+          ],
+        },
+      },
+    },
+    computerTurn: {
+      after: {
+        1000: {
+          target: 'determineWinner',
+          actions: [
+            assign({
+              computerChoice: () =>
+                choices[Math.floor(Math.random() * choices.length)],
+            }),
+          ],
+        },
+      },
+    },
+    determineWinner: {
+      always: {
+        target: 'end',
+        actions: [
+          assign({
+            winner: ({ context }) =>
+              determineWinner(context.playerChoice!, context.computerChoice!),
+          }),
+        ],
+      },
+    },
+    end: {
+      on: {
+        PLAY_AGAIN: {
+          target: 'start',
+          actions: [assign(initialContext)],
+        },
+      },
+    },
+  },
+})
+
+export default machine
